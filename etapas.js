433fb58f2211704ab2b9e13366637e0e568a9b7e
Lucas <lucas.fertony@gmail.com>
2021-08-12 01:08:18 -0300

    evento onclic iniciado

---
+++ etapas.js
@@ -0,0 +1,72 @@
+let etapas = [
+{
+    titulo: 'VEREADOR',
+    numeros: 5,
+    candidatos: [
+        {
+            numero: 11111,
+            nome: 'Goku Kakaroto',
+            patido: 'DBZ',
+            fotos:[
+                {
+                    url: 'goku.png', legenda: 'Vereador'
+                }
+            ]
+        },
+        {
+            numero: 22222,
+            nome: 'Deidara Explosion',
+            patido: 'Aktsuki',
+            fotos:[
+                {
+                    url: 'deidara.png', legenda: 'Vereador'
+                }
+            ]
+        },
+        {
+            numero: 33333,
+            nome: 'Zetsu Traidor',
+            patido: 'Aktsuki',
+            fotos:[
+                {
+                    url: 'zetsu.png', legenda: 'Vereador'
+                }
+            ]
+        }
+    ]
+
+},
+{
+    titulo: 'PREFEITO',
+    numeros: 2,
+    candidatos: [
+        {
+            numero: 11,
+            nome: 'Itachi Kai',
+            patido: 'Taka',
+            fotos:[
+                {
+                    url: 'itachi.png', legenda: 'Prefeito'
+                },
+                {
+                    url: 'kisame.png', legenda: 'Vice-Prefeito', small: true
+                }
+            ]
+        },
+        {
+            numero: 11,
+            nome: 'Pain Nagatao',
+            patido: 'Akatsuki',
+            fotos:[
+                {
+                    url: 'pain.png', legenda: 'Prefeito'
+                },
+                {
+                    url: 'konan.png', legenda: 'Vice-Prefeito', small: true
+                }
+            ]
+        }
+    ]
+
+}
+]
